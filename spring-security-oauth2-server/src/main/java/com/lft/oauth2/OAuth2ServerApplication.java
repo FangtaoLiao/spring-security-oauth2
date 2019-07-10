2acfe3e737fd86ba9ea6e6208a2fc9b5f23bed96
liaofangtao <fangtao.liao@foxmail.com>
2019-07-10 11:18:02 +0800

    创建项目

---
+++ spring-security-oauth2-server/src/main/java/com/lft/oauth2/OAuth2ServerApplication.java
@@ -0,0 +1,19 @@
+package com.lft.oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>OAuth2ServerApplication</p>
+ * <p>Description:</p>
+ *
+ * @author LiaoFangtao
+ * @date 2019/7/10 11:14
+ */
+@SpringBootApplication
+public class OAuth2ServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(OAuth2ServerApplication.class,args);
+  }
+}
